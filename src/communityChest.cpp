7f3619a0712267ec6e42671a62ee2db35f3abd88
EndymionMo <123090430@link.cuhk.edu.cn>
2024-12-01 20:07:06 +0800

    new

---
+++ src/communityChest.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include "communityChest.h"
+#include "player.h"
+
+CommunityChest::CommunityChest(std::string type,int amount,std::string sentence,int steps,int location)
+    : type(type),amount(amount),sentence(sentence),steps(steps),location(location) {}
+
+void CommunityChest::trigger(Player *player){
+    if(type=="change"){
+        
+    }
+    if(type=="advanceTo"){
+
+    }
+    if(type=="changeEach"){
+
+    }
+    if(type=="outOfJail"){
+
+    }
+    if(type=="goToJail"){
+
+    }
+    if(type=="repair"){
+
+    }
+}
+
+
