33ffbe7e830d47d9d0b08d6c04526947bf83ed62
sterversen <123090032@link.cuhk.edu.cn>
2024-12-13 00:47:54 +0800

    mm

--- src/property.cpp
+++ src/property.cpp
@@ -1,10 +1,11 @@
 #include "property.h"
+#include "slot.h"
 #include "iostream"
 
 Property::Property(std::string name, int price, int type)
-    : name(name), price(price), owner(nullptr), level(0), type(0){}
+    : name(name), price(price), owner(nullptr), level(0), Type(type){}
 
-//type数字对应：1-棕 2-浅蓝 3-浅红 4-橙 5-深红 6-黄 7-绿 8-深蓝 9-火车站 10-水电公司
+//Type数字对应：1-棕 2-浅蓝 3-浅红 4-橙 5-深红 6-黄 7-绿 8-深蓝 9-火车站 10-水电公司
 
 std::string Property::getName() const {
     return name;
@@ -34,16 +35,47 @@ void Property::upgrade() {
     ++level;
 }
 
-void Property::changePrice(int NewPrice) {
-    price = NewPrice;
-}
 
 void Property::payRent(Player* p){
+    // 确认地产是否有所有者
+    Player* owner = getOwner();
+    if (owner == nullptr || owner == p) {
+        // 如果没有所有者或者玩家自己拥有该地产，则不收费
+        return;
+    }
+    int numOfSameType = 0;
+    for (Property* index : slots) {
+        if (index->getType() == Type && index->getOwner() == owner) {
+            ++numOfSameType;
+        }
+    }// 记录拥有几个同色地块
 
+    int rent = 0.1 * price * (1 + level) * (0.5 + numOfSameType * 0.5);
+    owner->updateMoney(rent);
+    p->updateMoney(-rent);
 }
 
 void Property::utilitypayRent(Player* p){
-    p->updateMoney(- p->getlaststep() * 4);
+     // 确认水电是否有所有者
+    Player* owner = getOwner();
+    if (owner == nullptr || owner == p) {
+        // 如果没有所有者或者玩家自己拥有该地产，则不收费
+        return;
+    }
+    int numOfSameType = 0;
+    for (Property* index : slots) {
+        if (index->getType() == Type && index->getOwner() == owner) {
+            ++numOfSameType;
+        }
+    }// 记录拥有几个同色地块
+
+    if (numOfSameType == 1) {
+        p->updateMoney(- p->getlaststep() * 4);
+        owner->updateMoney( p->getlaststep() * 4);
+    }else {
+        p->updateMoney(- p->getlaststep() * 10);
+        owner->updateMoney( p->getlaststep() * 10);
+    }
 }
 
 void Property::stationpayRent(Player* p) {
